bd73c02ad0ebb7acd801c0de21e381af16bb9cc3
kaguna <karyorkir@gmail.com>
2017-11-24 00:27:44 +0300

    [Fix] Fix the bugs in the models file

---
+++ app/models.py
@@ -0,0 +1,105 @@
+# creates the user, categories and recipes tables schema
+
+from app import db
+
+# import the db connection from the app/__init__.py
+
+class Users(db.Model):
+    """ This class represents the users table"""
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                           onupdate=db.func.current_timestamp())
+    categories = db.relationship(
+        'Categories', order_by='categories.id', cascade="all, delete-orphan")
+
+    def __init__(self, email):
+        """initialize with user email."""
+        self.email = email
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Users.query.all()
+
+    def __repr__(self):
+        return "<Users: {}>".format(self.email)
+
+
+class Categories(db.Model):
+    """ This class represents the recipe categories table"""
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), unique=True)
+    category_owner_email = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                           onupdate=db.func.current_timestamp())
+    recipes = db.relationship(
+        'Recipes', order_by='recipes.id', cascade="all, delete-orphan")
+
+    def __init__(self, category_name):
+        """initialize with user email."""
+        self.category_name = category_name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Categories.query.all()
+
+    def __repr__(self):
+        return "<Categories: {}>".format(self.category_name)
+
+
+class Recipes(db.Model):
+    """ This class represents the recipes table"""
+
+    __tablename__ = 'recipes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name = db.Column(db.String(100), unique=True)
+    recipe_description = db.Column(db.String(255))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(),
+                           onupdate=db.func.current_timestamp())
+
+    def __init__(self, recipe_name):
+        """initialize with user email."""
+        self.recipe_name = recipe_name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return Recipes.query.all()
+
+    def __repr__(self):
+        return "<Recipes: {}>".format(self.recipe_name)
