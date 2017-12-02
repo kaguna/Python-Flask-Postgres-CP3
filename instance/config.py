a91dbea3702d946b3ead7775eae07eeb77657ac2
kaguna <karyorkir@gmail.com>
2017-12-02 13:06:38 +0300

    [Chore] change the database and add a line to activate the virtual environment

--- instance/config.py
+++ instance/config.py
@@ -19,7 +19,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sembe@localhost/test_db'
     DEBUG = True
 
 
