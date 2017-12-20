db6a23de34ae95d2062c8a3bacc5809950a2d939
James Kariuki <karyorkir@gmail.com>
2017-12-20 14:34:01 +0300

    [Fix] fix test category creation

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -1,8 +1,9 @@
 # This file will have all the categories CRUD operations tested
 
 import unittest
-import os
+import json
 from app import create_app, db
+from flask import request
 
 
 class CategoriesTestCase(unittest.TestCase):
@@ -11,6 +12,8 @@ class CategoriesTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
+        self.register_user = {'email': 'jim@gmail.com', 'username': 'jim', 'password': 'pass123'}
+        self.login_user= {'email': 'jim@gmail.com', 'password': 'pass123'}
         self.categories = {'category_name': 'Lunch'}
 
         # binds the app to the current context
@@ -18,9 +21,17 @@ class CategoriesTestCase(unittest.TestCase):
             # create all tables
             db.create_all()
 
+        self.client().post('auth/user/', data=self.register_user)
+        self.login_details = self.client().post('auth/login/', data=self.login_user)
+        self.access_token = json.loads(self.login_details.data.decode())['access_token']
+
+    def define_header(self, access_token):
+        access_token = request.headers.get('x-access-token')
+        return access_token
+
     def test_category_creation(self):
         """Test API can create a category (POST request)"""
-        res = self.client().post('/categories/', data=self.categories)
+        res = self.client().post('/categories/',headers ={'x-access-token':self.access_token}, data=self.categories)
         self.assertEqual(res.status_code, 201)
         self.assertIn('Category created successfully', str(res.data))
 
