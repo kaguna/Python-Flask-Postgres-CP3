dfab95653e03f3dd110f21836d624628cdabd8c5
James Kariuki <karyorkir@gmail.com>
2017-12-20 14:42:32 +0300

    [Refactor] refactor code

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -3,7 +3,6 @@
 import unittest
 import json
 from app import create_app, db
-from flask import request
 
 
 class CategoriesTestCase(unittest.TestCase):
@@ -13,7 +12,7 @@ class CategoriesTestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
         self.register_user = {'email': 'jim@gmail.com', 'username': 'jim', 'password': 'pass123'}
-        self.login_user= {'email': 'jim@gmail.com', 'password': 'pass123'}
+        self.login_user = {'email': 'jim@gmail.com', 'password': 'pass123'}
         self.categories = {'category_name': 'Lunch'}
 
         # binds the app to the current context
@@ -24,14 +23,10 @@ class CategoriesTestCase(unittest.TestCase):
         self.client().post('auth/user/', data=self.register_user)
         self.login_details = self.client().post('auth/login/', data=self.login_user)
         self.access_token = json.loads(self.login_details.data.decode())['access_token']
-
-    def define_header(self, access_token):
-        access_token = request.headers.get('x-access-token')
-        return access_token
-
+    
     def test_category_creation(self):
         """Test API can create a category (POST request)"""
-        res = self.client().post('/categories/',headers ={'x-access-token':self.access_token}, data=self.categories)
+        res = self.client().post('/categories/', headers={'x-access-token': str(self.access_token)}, data=self.categories)
         self.assertEqual(res.status_code, 201)
         self.assertIn('Category created successfully', str(res.data))
 
