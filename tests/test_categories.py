b906a256f99af60c03fc0f524a333c44052664d5
James Kariuki <karyorkir@gmail.com>
2017-12-22 09:54:59 +0300

    [Feature] test the categories operations
    This will test the category creation, update and delete.
    
    [Finishes #153848731] test the category operations

--- tests/test_categories.py
+++ tests/test_categories.py
@@ -24,11 +24,103 @@ class CategoriesTestCase(unittest.TestCase):
         login_details = self.client.post('/auth/login/', data=login_user)
         self.access_token = json.loads(login_details.data.decode())['access_token']
 
+    """Test category creation(POST)
+    """
     def test_category_creation(self):
-        """Test API can create a category (POST request)"""
+        """Test API can create a category (POST request)
+        """
         res = self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
         self.assertEqual(res.status_code, 201)
         self.assertIn('Category created successfully', str(res.data))
+
+    def test_category_existence(self):
+        """Test if API can create a category (POST request) that exists
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        res = self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 409)
+        self.assertIn('Category exists!', str(res.data))
+
+    def test_invalid_category_name(self):
+        """Test if API can create a category with an invalid name
+        """
+        categories = {'category_name': '$%#%^&&'}
+        res = self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=categories)
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('Invalid category name given', str(res.data))
+
+    def test_empty_category_name(self):
+        """Test if API can create a category with name not provided
+        """
+        self.categories = {'category_name': ''}
+        res = self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 422)
+        self.assertIn('Please fill all the fields', str(res.data))
+
+    """Test category name update(PUT)
+    """
+    def test_category_update(self):
+        """Test API can update a category (PUT request)
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        new_category_name = {'category_name': 'Breakfast'}
+        res = self.client.put('/categories/1', headers={'x-access-token': self.access_token}, data=new_category_name)
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('Category updated successfully', str(res.data))
+
+    def test_category_update_existence(self):
+        """Test if API can update a category (PUT request) that exists
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.client.put('/categories/1', headers={'x-access-token': self.access_token}, data=self.categories)
+        res = self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 409)
+        self.assertIn('Category exists!', str(res.data))
+
+    def test_invalid_update_category_name(self):
+        """Test if API can update a category with an invalid name
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        categories = {'category_name': '$%#%^&&'}
+        res = self.client.put('/categories/1', headers={'x-access-token': self.access_token}, data=categories)
+        self.assertEqual(res.status_code, 400)
+        self.assertIn('Invalid category name given', str(res.data))
+
+    def test_update_non_existence_category(self):
+        """Test API can update a category which does not exist (UPDATE request)
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        res = self.client.delete('/categories/3', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Category not found', str(res.data))
+
+    def test_empty_update_category_name(self):
+        """Test if API can update a category with name not provided
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.categories = {'category_name': ''}
+        res = self.client.put('/categories/1', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 422)
+        self.assertIn('Please fill all the fields', str(res.data))
+
+    """Test category deletion
+    """
+
+    def test_category_deletion(self):
+        """Test API can delete a category (DELETE request)
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        res = self.client.delete('/categories/1', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('Category deleted', str(res.data))
+
+    def test_delete_non_existence_category(self):
+        """Test API can delete a category which does not exist (DELETE request)
+        """
+        self.client.post('/categories/', headers={'x-access-token': self.access_token}, data=self.categories)
+        res = self.client.delete('/categories/3', headers={'x-access-token': self.access_token}, data=self.categories)
+        self.assertEqual(res.status_code, 401)
+        self.assertIn('Category not found', str(res.data))
 
     def tearDown(self):
         """teardown all initialized variables."""
