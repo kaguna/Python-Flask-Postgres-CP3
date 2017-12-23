fbcbfff6f58607d12cfbaa5ccc3ed8e26928466d
James Kariuki <karyorkir@gmail.com>
2017-12-23 23:45:58 +0300

    [Feature] edit the recipe name
    This will update the recipe name of a given category.
    
    [Finishes #153869462] edit and update the recipe name

--- classes/recipes.py
+++ classes/recipes.py
@@ -14,6 +14,7 @@ class NonFilteredRecipesManipulations(MethodView):
     decorators = [token_required]
 
     def post(self, user_in_session, category_id):
+        # This method will create and save a recipe
         recipename = str(request.data.get('recipe_name', '')).strip()
         recipeprocedure = str(request.data.get('recipe_procedure', '')).strip()
         regexrecipe_name = "[a-zA-Z0-9- .]"
@@ -35,6 +36,7 @@ class NonFilteredRecipesManipulations(MethodView):
         return make_response(jsonify({'message': 'Category not found'})), 404
 
     def get(self, user_in_session, category_id):
+        # This method will retrieve all the recipes under the category given
         category_recipes = Recipes.get_all(category_id)
         results = []
         for recipes in category_recipes:
@@ -51,14 +53,16 @@ class NonFilteredRecipesManipulations(MethodView):
         response.status_code = 200
         return response
 
+
 class FilteredRecipesManipulations(MethodView):
     """This will handle the GET, PUT and DELETE methods with parameters
     """
     decorators = [token_required]
 
     def get(self, user_in_session, category_id, recipe_id):
+        # Retrieve a specific recipe from cetegory given
         single_recipe = Recipes.query.filter_by(category_id=category_id, id=recipe_id).first()
-        if not recipe_id:
+        if single_recipe:
             one_recipe = {
                 'id': single_recipe.id,
                 'recipe_name': single_recipe.recipe_name,
@@ -70,6 +74,26 @@ class FilteredRecipesManipulations(MethodView):
             response = jsonify(one_recipe)
             response.status_code = 200
             return response
+        return make_response(jsonify({'message': 'Recipe not found'})), 404
+
+    def put(self, user_in_session, category_id, recipe_id):
+        # Edit the recipe name from a given category
+        recipename = str(request.data.get('recipe_name', '')).strip()
+        regexrecipe_name = "[a-zA-Z0-9- .]"
+        recipe = Recipes.query.filter_by(id=recipe_id, category_id=category_id).first()
+        recipe_existence = Recipes.query.filter_by(recipe_name=recipename).first()
+        if recipe:
+            if recipename:
+                if re.search(regexrecipe_name, recipename):
+                    if not recipe_existence:
+
+                        recipe.recipe_name = recipename
+                        recipe.save()
+
+                        return make_response(jsonify({'message': 'Recipe updated successfully'})), 201
+                    return make_response(jsonify({'message': 'Recipe exists!'})), 409
+                return make_response(jsonify({'message': 'Invalid recipe name given'})), 400
+            return make_response(jsonify({'message': 'Please fill all the fields'})), 422
         return make_response(jsonify({'message': 'Recipe not found'})), 404
 
 
