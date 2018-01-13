d1e03105721980e90a883b0540bbeebeb47b0cbd
James Kariuki <karyorkir@gmail.com>
2018-01-14 00:07:29 +0300

    [Feature] add the route to the index of the apidocs

--- run.py
+++ run.py
@@ -1,8 +1,17 @@
 # runs the API
 import os
+from flask import redirect
 from app import create_app
 
 app = create_app(os.getenv('APP_SETTINGS'))
+
+
+@app.route('/')
+def rundoc():
+    """method to run app documentation
+    """
+    return redirect('/apidocs/')
+
 
 if __name__ == '__main__':
     app.run(port=5000)
