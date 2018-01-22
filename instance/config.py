1ef186664df4c43688fbce0127fa24db14dc72ad
James Kariuki <karyorkir@gmail.com>
2018-01-22 14:11:46 +0300

    [Fix] fix database url

--- instance/config.py
+++ instance/config.py
@@ -19,7 +19,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:sembe@localhost:5432/test_db'
     DEBUG = True
 
 
