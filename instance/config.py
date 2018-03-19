9c63368613de79438ae8976b2c86e29593833724
James Kariuki Kaguna <karyorkir@gmail.com>
2018-03-19 13:04:00 +0300

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -8,17 +8,11 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
-    
-    if os.environ.get('DATABASE_URL') is None:
-        SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/flask_api'
-    else:
-        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 
 class TestingConfig(Config):
